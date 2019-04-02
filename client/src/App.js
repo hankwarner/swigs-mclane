23eb81f046e1b4e0a452ebcb5638835f247775bb
hankwarner <40283259+hankwarner@users.noreply.github.com>
2019-04-01 22:58:21 -0400

    nav bar implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import { DesktopNavBar, MobileNavBar } from './components/NavBar';
+import Landing from './components/Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <DesktopNavBar></DesktopNavBar>
+        <MobileNavBar></MobileNavBar>
+        <Landing></Landing>
       </div>
     );
   }

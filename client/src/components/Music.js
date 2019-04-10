e05fda8ac25a205144c62120abc84b17e3b9610a
hankwarner <40283259+hankwarner@users.noreply.github.com>
2019-04-09 23:26:19 -0400

    rendering songs in album component

--- client/src/components/Music.js
+++ client/src/components/Music.js
@@ -5,7 +5,9 @@ import {
     Image,
     Icon,
     Card,
-    Header
+    Header,
+    List,
+    Container
 } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { getMusic } from '../actions/musicActions';
@@ -51,31 +53,36 @@ class Music extends Component {
                             </Grid.Column>
                         ))}
                     </Grid.Row>
+                </Grid>
+                <Grid>
                     <Grid.Row centered>
                         <Header as='h1' inverted color='grey' id="header">
                             Albums
                         </Header>
                     </Grid.Row>
                     <hr></hr>
-                    <Grid.Row centered columns={3}>
-                        {albums.map((album) => (
-                            <Grid.Column>
-                                <div className="singles">
-                                    <Card>
-                                        <Image href={album.soundcloudUrl} src={images(album.albumCoverUrl)} />
-                                        <Card.Content>
-                                            <Card.Header>{album.title}</Card.Header>
-                                            <Card.Meta>{album.artist}</Card.Meta>
-                                            <Card.Description>{album.songs}</Card.Description>
-                                        </Card.Content>
-                                        <Card.Content extra>
-                                            <a href={album.soundcloudUrl}><Icon name='soundcloud' />Listen on Soundcloud</a>
-                                        </Card.Content>
-                                    </Card>
-                                </div>
-                            </Grid.Column>
-                        ))}
-                    </Grid.Row>
+                    {albums.map(album => {
+                        return (
+                            <Grid.Row columns={2}>
+                                <Grid.Column width={8}>
+                                    <Image href={album.soundcloudUrl} src={images(album.albumCoverUrl)} />
+                                </Grid.Column>
+                                <Grid.Column width={8}>
+                                    <List divided verticalAlign='middle'>
+                                        {album.songs.map(song => {
+                                            return (
+                                                <List.Item>
+                                                    <List.Content>
+                                                        <List.Header>{song}</List.Header>
+                                                    </List.Content>
+                                                </List.Item>
+                                            )
+                                        })}
+                                    </List>
+                                </Grid.Column>
+                            </Grid.Row> 
+                        )
+                    })}
                 </Grid>
                 {this.props.loading ? <Loader active inline='centered' /> : null }
             </div>

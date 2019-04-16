cdfd5a0dc8a0005806afeb2ed75ef3645518d19c
hankwarner <40283259+hankwarner@users.noreply.github.com>
2019-04-15 20:14:04 -0400

    updated landing page grid with latest news column

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,7 +4,8 @@ import {
     Grid,
     Container,
     Image,
-    Header
+    Header,
+    GridColumn
 } from 'semantic-ui-react'
 import { connect } from 'react-redux';
 import { setInstagramFeed, setInstagramLoading } from '../actions/landingActions';
@@ -21,7 +22,7 @@ class Landing extends Component {
             userId: instagramKeys.userId,
             clientId: instagramKeys.instagramApiKey,
             accessToken: instagramKeys.accessToken,
-            // if valid data is returned from Instagram, store data in Redux store
+            // if valid data is returned from Instagram, store data in Redux
             success: (res) => {
                 this.props.setInstagramFeed(res.data);
             }
@@ -34,27 +35,45 @@ class Landing extends Component {
 
         return (
             <div>
-                <Responsive as={Grid}>
+                <Responsive as={Grid} columns='two'>
                     {/* <Image src={images('./my_city.png')} size='big' /> */}
                     <Grid.Row centered>
-                        <Header size='huge' inverted color='grey' id="instagram-header">
-                            Instagram
-                        </Header>
+                        <Grid.Column width={8}>
+                            <Header size='huge' inverted color='grey' id="instagram-header">
+                                Latest News
+                            </Header>
+                            <hr className="landing-side-panel-dividers"></hr>
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Header size='huge' inverted color='grey' id="instagram-header">
+                                Instagram
+                            </Header>
+                            <hr className="landing-side-panel-dividers"></hr>
+                        </Grid.Column>
                     </Grid.Row>
-                    <hr className="landing-side-panel-dividers"></hr>
-                    <Grid.Row columns={2} id="instagram-feed">
-                        {instagramFeed.map(photo => {
-                            return (
-                                <div key={photo.id}>
-                                    <Grid.Column as={Responsive} minWidth={768}>
-                                        <Image href={photo.link} src={photo.images.thumbnail.url} id="instagram-photos" />
-                                    </Grid.Column>
-                                    <Grid.Column as={Responsive} maxWidth={767}>
-                                        <Image size='medium' href={photo.link} src={photo.images.standard_resolution.url} id="instagram-photos" />
-                                    </Grid.Column>
-                                </div>
-                            )
-                        })}
+                    <Grid.Row columns="two">
+                        <Grid.Column width={10} as={Responsive} minWidth={768}>
+                            <Image src={images('./my_city.png')} size='big' />
+                            <span>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </span>
+                            <hr className="landing-side-panel-dividers"></hr>
+                            <Image src={images('./my_city.png')} size='big' />
+                            <span>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </span>
+                        </Grid.Column>
+                        <Grid.Column width={6}>
+                            <Grid columns="three" id="instagram-feed">
+                                {instagramFeed.map(photo => {
+                                    return (
+                                        <Grid.Column as={Responsive} minWidth={768}>
+                                            <Image href={photo.link} src={photo.images.thumbnail.url} id="instagram-photos" />
+                                        </Grid.Column>
+                                    )
+                                })}
+                            </Grid>
+                        </Grid.Column>
                     </Grid.Row>
                 </Responsive>
             </div>

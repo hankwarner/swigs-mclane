1dbad6850dc40f82cc9d78a781f115721a344f12
hankwarner <40283259+hankwarner@users.noreply.github.com>
2019-04-22 20:36:12 -0400

    preparing for first heroku deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const singles = require('./routes/api/singles');
 const albums = require('./routes/api/albums');
 const twitter = require('./routes/api/twitter');
@@ -23,6 +24,16 @@ mongoose.connect(db)
 app.use('/api/singles', singles);
 app.use('/api/albums', albums);
 app.use('/api/twitter', twitter);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000;
 
